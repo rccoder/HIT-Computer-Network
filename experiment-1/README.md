34b7eb1b19ef74ca61ee04c2cc57ca587476b96f
rccoder <rccoder.net@gmail.com>
2016-04-28 22:28:23 +0800

    Add experiment-1

---
+++ experiment-1/README.md
@@ -0,0 +1,16 @@
+# HTTP 代理服务器的设计与实现
+
++ HTTP 走普通代理
+
+![image](https://cloud.githubusercontent.com/assets/7554325/14889141/20706c78-0d90-11e6-8d31-c1112baa12c4.png)
+
+
++ HTTPS 走隧道模式代理
+
+![image](https://cloud.githubusercontent.com/assets/7554325/14889155/361bb8fc-0d90-11e6-931b-a5a297d22f9b.png)
+
+### 参考资料
+
++ [Node API——net connect](https://nodejs.org/api/net.html#net_event_connect)
++ [在Node.js中使用TCP套接字编程——CNode社区](https://cnodejs.org/topic/4fb1c1fd1975fe1e1310490b)
++ [HTTP代理原理实现——Jerry Qu](https://imququ.com/post/web-proxy.html)
---
+++ experiment-1/app.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const http = require('http');
+const url = require('url');
+const net = require('net');
+
+const hostname = '127.0.0.1';
+const port = '8888';
+
+
+// 隧道代理
+const connect = (creq, csock) => {
+    
+    console.log(creq.headers);
+    
+    const u = url.parse('http://' + creq.url);
+    
+    const psock = net.connect(u.port, u.hostname, () => {
+        csock.write('HTTP/1.1 200 Connection Established\r\n\r\n');
+        psock.pipe(csock);
+    }).on('error', (e) => {
+        csock.end();
+    });
+    
+    csock.pipe(psock);
+}
+
+// 普通代理
+const request = (creq, cres) => {
+    
+    console.log(creq.headers);
+    
+    const u = url.parse(creq.url);
+    
+    const options = {
+        hostname: u.hostname,
+        port: u.port || 80,
+        path: u.path,
+        method: u.method,
+        headers: u.headers
+    };
+    
+    const purposeReq = http.request(options, (purposeRes) => {
+        
+        cres.writeHead(purposeRes.statusCode, purposeRes.headers);
+        purposeRes.pipe(cres);
+        
+    }).on('error', (e) => {
+        cres.end();
+    });
+    
+    creq.pipe(purposeReq);
+};
+
+const proxy = http.createServer()
+    .on('request', request)
+    .on('connect', connect);
+    
+proxy.listen(port, hostname, () => {
+    console.log(`Proxy run in ${hostname}:${port}`);
+});
